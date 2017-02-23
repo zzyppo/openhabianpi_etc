26ab9bd9790c15cf0370a4f1105850a3c5cf945c
pi <pi@openHABianPi>
2017-02-23 20:18:41 +0100

    saving uncommitted changes in /etc prior to apt run

--- openhab2/scripts/bt_device_ping.sh
+++ openhab2/scripts/bt_device_ping.sh
@@ -4,9 +4,8 @@
 case $1 in
    1) MAC="48:4B:AA:62:08:62" ;;
    2) MAC="6C:72:E7:36:10:F1" ;;
+   3) MAC="14:BB:6E:89:FB:74" ;;
 esac
-
-echo i ran >> /etc/openhab2/bt_out.txt
  
 if sudo /usr/bin/l2ping -c 1 $MAC &> /dev/null
    then
